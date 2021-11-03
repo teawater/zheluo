5d75f8a372453141ab515d04576614ed6fc7b4a8
Hui Zhu <teawater@antgroup.com>
2021-11-03 15:55:09 +0800

    First commit can output
    qemu-system-x86_64 -display none -smp 1 -m 2G -chardev stdio,id=gnc0
    -device pci-serial,chardev=gnc0 -kernel zheluo.elf -no-acpi -machine accel=kvm
    
    Signed-off-by: Hui Zhu <teawater@antgroup.com>

---
+++ init/main.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <multiboot.h>
+#include <elf.h>
+#include <page.h>
+
+#define HALT	asm volatile ("hlt")
+
+extern const void kernel_start;
+extern const void kernel_end;
+extern const void bss_start;
+extern const void bss_end;
+
+void main(void)
+{
+	// initialize .bss section
+	memset((void*)&bss_start, 0, ((size_t) &bss_end - (size_t) &bss_start));
+
+	uart_init();
+
+	uart_puts("123\n");
+
+	while(1) { HALT; }
+}
